ae7abd29b33645bff3803de9d59afaadd67e760c
Jarret Angbazo <jarretangbazo@gmail.com>
2025-02-09 16:52:51 -0600

    Reorganize folder
    Reorganize folder

---
+++ code/00_Extract_Data.py
@@ -0,0 +1,4 @@
+"""
+Automate web scraping/data extraction using 'requests' and 'beautifulsoup'. Extract player performance data following latest PGA Tour Tournament.
+"""
+
---
+++ code/01_Data_Process.r
@@ -0,0 +1,7 @@
+# Clean
+
+# Exploratory Data Analysis -- visualization, frequencies, other checks, ANOVA, etc.
+
+# Feature Engineering, Model Development/Evaluation
+
+# Final Analysis
